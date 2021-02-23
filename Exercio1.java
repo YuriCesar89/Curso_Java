1f7d98a57ea34e153f63c6ee6b1fa993aa940564
YuriCesar89 <74461058+YuriCesar89@users.noreply.github.com>
2021-02-23 11:36:26 -0300

    Add files via upload
    Olá gente, iniciando meu aprendizado com a linguagem Java, adquiri esse curso pela plataforma Udemy, curso ministrado pelo professor @Nélio Alves.

---
+++ Exercio1.java
@@ -0,0 +1,38 @@
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercio1 {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+
+		while (x != 0 && y != 0){
+			
+			if (x > 0 && y > 0) {
+				System.out.println("primeiro");
+			}
+			else if (x < 0 && y > 0) {
+				System.out.println("quarto");
+			}
+			else if (x > 0 && y < 0) {
+				System.out.println("segundo");
+			}
+			else {
+				System.out.println("terceiro");
+			}
+
+			x = sc.nextInt();
+			y = sc.nextInt();
+
+		}
+
+		sc.close();
+	}
+
+}
